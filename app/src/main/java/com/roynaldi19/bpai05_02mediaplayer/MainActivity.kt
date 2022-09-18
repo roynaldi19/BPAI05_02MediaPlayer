551c32cf8dfecd1478cb1e7102e6a97d90a4855c
roynaldi19 <roynaldi19@gmail.com>
2022-09-18 15:56:10 +0700

    Latihan MediaPlayer

--- app/src/main/java/com/roynaldi19/bpai05_02mediaplayer/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/bpai05_02mediaplayer/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.roynaldi19.bpai05_02mediaplayer
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.roynaldi19.bpai05_02mediaplayer.databinding.ActivityMainBinding
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
+    private var isReady: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPlay.setOnClickListener {
+            if (!isReady) {
+                mediaPlayer?.prepareAsync()
+            } else {
+                if (mediaPlayer?.isPlaying as Boolean) {
+                    mediaPlayer?.pause()
+                } else {
+                    mediaPlayer?.start()
+                }
+            }
+        }
+
+        binding.btnStop.setOnClickListener {
+            if (mediaPlayer?.isPlaying as Boolean || isReady) {
+                mediaPlayer?.stop()
+                isReady = false
+            }
+        }
+
+        init()
+    }
+
+    private fun init() {
+        mediaPlayer = MediaPlayer()
+        val attribute = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+        mediaPlayer?.setAudioAttributes(attribute)
+
+        val afd = applicationContext.resources.openRawResourceFd(R.raw.guitar_background)
+        try {
+            mediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        mediaPlayer?.setOnPreparedListener {
+            isReady = true
+            mediaPlayer?.start()
+        }
+        mediaPlayer?.setOnErrorListener { _, _, _ -> false }
     }
 }
